8e9d028495d93fdacdc153f5e1fa9a34cbcb90e9
Matheus Torres <torresmiranda.rj@gmail.com>
2022-12-14 11:49:43 -0300

    fix: make use of global pipes

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
   
-  app.use(
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true
