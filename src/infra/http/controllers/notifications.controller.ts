0c5b77585ff75ddfd1f31f6deec62f0805117d78
Matheus Torres <torresmiranda.rj@gmail.com>
2022-12-16 12:10:59 -0300

    feat: use NotificationViewModel

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -4,17 +4,18 @@ import { Notification } from '@/src/app/entities/notification.entity';
 import { CreateNotificationDto } from '@/src/infra/http/dtos/create-notification.dto';
 
 import { SendNotificationUseCase } from '@/src/app/use-cases/sendNotification.use-case';
+import { NotificationToHTTP, NotificationViewModel } from '../view-models/notification.view-model';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(private sendNotification: SendNotificationUseCase) {}
 
   @Post()
-  public async create(@Body() body: CreateNotificationDto): Promise<{ notification: Notification }> {
+  public async create(@Body() body: CreateNotificationDto): Promise<{ notification: NotificationToHTTP }> {
     const { notification } = await this.sendNotification.execute(body);
 
     return {
-      notification
+      notification: NotificationViewModel.toHTTP(notification)
     }
   }
 }
