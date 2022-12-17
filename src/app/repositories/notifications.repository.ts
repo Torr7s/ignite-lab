14c56b9ae2297d0f763bcf76d59757b3fe89c66b
Matheus Torres <torresmiranda.rj@gmail.com>
2022-12-17 17:18:03 -0300

    feat: add findById and save abstract methods

--- src/app/repositories/notifications.repository.ts
+++ src/app/repositories/notifications.repository.ts
@@ -2,4 +2,6 @@ import { Notification } from '../entities/notification.entity';
 
 export abstract class NotificationsRepository {
   public abstract create(notification: Notification): Promise<void>;
+  public abstract findById(notificationId: string): Promise<Notification|null>;
+  public abstract save(notification: Notification): Promise<void>;
 }
