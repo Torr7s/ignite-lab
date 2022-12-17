e4869f83617debf313fd72b32e2a92758a5104e9
Matheus Torres <torresmiranda.rj@gmail.com>
2022-12-17 17:18:26 -0300

    chore: implement new abstract methods

--- tests/repositories/notifications.repository.memo.ts
+++ tests/repositories/notifications.repository.memo.ts
@@ -7,4 +7,22 @@ export class NotificationsRepositoryInMemory implements NotificationsRepository 
   public async create(notification: Notification): Promise<void> {
     this.notifications.push(notification);
   }
+
+  public async findById(notificationId: string): Promise<Notification|null> {
+    const notification: Notification|undefined = this.notifications.find(
+      (item: Notification): boolean => item.id === notificationId
+    );
+
+    return notification ?? null;
+  }
+
+  public async save(notification: Notification): Promise<void> {
+    const notificationIndex: number = this.notifications.findIndex(
+      (item: Notification): boolean => item.id === notification.id
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
 }
